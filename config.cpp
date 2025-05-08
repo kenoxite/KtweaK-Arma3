8c3d04298b1b18e2e2558f6da8db2a1a23d03328
kenoxite <kenoxite@gmail.com>
2025-05-08 19:59:42 +0200

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,6 +1,6 @@
-#define VERSION 2.3.1 // Should be a floating-point number (1 separator)
-#define VERSION_STR "2.3.1" // Since CBA v3.13.0
-#define VERSION_AR {2,3,1}
+#define VERSION 2.3.2 // Should be a floating-point number (1 separator)
+#define VERSION_STR "2.3.2" // Since CBA v3.13.0
+#define VERSION_AR {2,3,2}
 
 class CfgPatches
 {
